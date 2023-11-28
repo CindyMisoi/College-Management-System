9ab24c3103e597d39b7fdab91f6fa00de37bc894
CindyMisoi <crmisoi2k@gmail.com>
2023-11-28 16:58:30 +0300

    toast added to faculty

--- client/src/Screens/Admin/Faculty.jsx
+++ client/src/Screens/Admin/Faculty.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { toast } from "react-hot-toast";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Heading from "../../components/Heading";
 import axios from "axios";
 import apiServer from "../../config/apiServer";
@@ -799,6 +800,7 @@ const Faculty = () => {
           )}
         </div>
       )}
+      <ToastContainer/>
     </div>
   );
 };
